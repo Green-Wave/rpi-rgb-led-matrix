38c354a5101d1ed92f6c11e7fbbf4a8f0981617a
Thorben Jensen <jensen.thorben@gmail.com>
2019-09-27 15:31:52 +0100

    cleaned code, API requests and countdown working, adding readme

--- bindings/python/samples/greenwave.py
+++ bindings/python/samples/greenwave.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 from samplebase import SampleBase
 import time
+import json
+
+import requests
+
 
 RESPONSE_MOCKUP = {
     "id_light": 1,
     "is_green": True,
     "is_red": False,
-    "seconds_phase_left": 4.1,
+    "seconds_phase_left": 44.1,
     "seconds_phase_total": 50.0,
 }
 
@@ -16,6 +20,7 @@ class GrayscaleBlock(SampleBase):
 
     def run(self):
         while True:
+            # get phase timing
             print("Starting loop...")
             response = self.get_seconds_phase_left()
             # color LED matrix
@@ -33,28 +38,31 @@ class GrayscaleBlock(SampleBase):
             print()
 
     def get_seconds_phase_left(self) -> dict:
-        return RESPONSE_MOCKUP
+        print("Getting seconds left for :phase...")
+        response = requests.get("http://172.16.2.62/seconds_phase_left")
+        json_response = json.loads(response.text)
+        print(json_response)
+        return json_response
 
     def color(self, color, ratio_left):
         width = self.matrix.width  # 64
         height = self.matrix.height  # 32
-        brightness = 150  # 255 with red pulls too much power
-        width_on = (width) * ratio_left  # TODO: check calculation
+        if color == "green":
+            brightness = 255
+        else:
+            brightness = 150   # 255 with red pull too much power
+        width_on = width * ratio_left
 
         # draw 'ratio left'
         for y in range(0, height):
-            for x in range(int(width - width_on), width):
-                if color == "green":
-                    self.matrix.SetPixel(x, y, 0, brightness, 0)
-                elif color == "red": 
-                    self.matrix.SetPixel(x, y, brightness, 0, 0)
-
-        # draw frame
-        #widths_frame = [0, width]
-        #heights_frame = [0, height]
-        #for y in range(0, height):
-        #    for x in widths_frame:
-        #        self.matrix.SetPixel(x, y, brightness, brightness, 0)
+            for x in range(0, width):
+                if x >= (width - int(width_on)):
+                    if color == "green":
+                        self.matrix.SetPixel(x, y, 0, brightness, 0)
+                    elif color == "red": 
+                        self.matrix.SetPixel(x, y, brightness, 0, 0)
+                else:
+                    self.matrix.SetPixel(x, y, 0, 0, 0)
 
 
 # Main function
