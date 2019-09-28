21bf199ee1d2344ec9a18392902a589cb3c54153
Christian Römer <croemer@informationsfabrik.de>
2019-09-28 16:11:33 +0200

    fix logo

--- bindings/python/samples/greenwave-image.py
+++ bindings/python/samples/greenwave-image.py
@@ -38,137 +38,33 @@ class GrayscaleBlock(SampleBase):
         while True:
             # get phase timing
             print("Starting loop...")
-            # color, seconds_phase_left, seconds_phase_total = self.get_http_time()
-            try:
-                print("getting lora...")
-                color, seconds_phase_left, seconds_phase_total = self.get_lora_time()
-            except KeyboardInterrupt:
-                raise
-            except:
-                print("Unexpected error:", sys.exc_info()[0])
-                continue
-
-            # make last seconds empty frame
-            seconds_phase_left -= 5
-            seconds_phase_total -= 5
 
             # color LED matrix
             print("Coloring LED...")
-            ratio_left = seconds_phase_left / seconds_phase_total
-            self.color(color=color, ratio_left=ratio_left)
+            self.color()
             
             # sleep
             print("Sleeping ...")
-            time.sleep(0.1)
+            time.sleep(0.05)
             print("Loop done.")
             print()
 
-    def get_lora_time(self):
-        packet = rfm9x.receive(timeout=5)
-        if packet is None:
-            # Packet has not been received
-            print('Received nothing! Listening again...') 
-        else:
-            print('Received (raw bytes): {0}'.format(packet))
-            packet_text = str(packet, 'ascii')
-            print('Received (ASCII): {0}'.format(packet_text))
-            [green, seconds_left, seconds_total] = packet_text.split(";")
-            if green == "1":
-                color = "green"
-            else:
-                color = "red"
-            print(f"color: {color}")
-            print("seconds left:", seconds_left)
-            print("seconds total:", seconds_total)
-            # Also read the RSSI (signal strength) of the last received message and print it.
-            rssi = rfm9x.rssi
-            print('Received signal strength: {0} dB'.format(rssi))
-            return color, float(seconds_left), float(seconds_total)
-
-    def get_http_time(self):
-        print("Getting seconds left for :phase...")
-        response = requests.get("http://172.16.2.107/seconds_phase_left")
-        res_dict = json.loads(response.text)
-        print(res_dict)
-        if res_dict["is_green"]:
-            color = "green"
-        else:
-            color = "red"
-        seconds_phase_left = res_dict["seconds_phase_left"]
-        seconds_phase_total = res_dict["seconds_phase_total"]
-        time.sleep(0.5)
-        return color, seconds_phase_left, seconds_phase_total
-
-    def color(self, color, ratio_left, minipicture=0):
+    def color(self):
         width = self.matrix.width  # 64
         height = self.matrix.height  # 32
-         # if minipicture is None or minipicture <= 0 or minipicture >= 5:
-        if False:
-            if color == "green":
-                brightness = 155
-            else:
-                brightness = 100   # 255 with red pull too much power
-
-            # show normal filling
-            width_on = width * ratio_left
-
-            # draw 'ratio left'
-            for y in range(1, height - 1):
-                for x in range(1, width - 1):
-                    if x >= (width - int(width_on)):
-                        if color == "green":
-                            self.matrix.SetPixel(x, y, 0, brightness, 0)
-                        elif color == "red": 
-                            self.matrix.SetPixel(x, y, brightness, 0, 0)
-                    else:
-                        self.matrix.SetPixel(x, y, 0, 0, 0)
-            
-            # draw frame
-            for y in range(0, height):
-                if color == "green":
-                    self.matrix.SetPixel(0, y, 0, brightness, 0)
-                    self.matrix.SetPixel(width - 1, y, 0, brightness, 0)
-                elif color == "red": 
-                    self.matrix.SetPixel(0, y, brightness, 0, 0)
-                    self.matrix.SetPixel(width - 1, y, brightness, 0, 0)
-            for x in range(0, width):
-                if color == "green":
-                    self.matrix.SetPixel(x, 0, 0, brightness, 0)
-                    self.matrix.SetPixel(x, height - 1, 0, brightness, 0)
-                elif color == "red": 
-                    self.matrix.SetPixel(x, 0, brightness, 0, 0)
-                    self.matrix.SetPixel(x, height - 1, brightness, 0, 0)
-        else:
-            print("minipicture")
-            # minipicture == 1: muensterhack logo
-            # r, g, b = None, None, None
-            # cnt = 0
-            # for i in muensterhack:
-            #     if r is None:
-            #         r = i
-            #     elif g is None:
-            #         g = i
-            #     elif b is None:
-            #         b = i
-            #     else:
-            #         print(f"{cnt % width}  {cnt // height}  {r}  {g}  {b}")
-            #         self.matrix.SetPixel(cnt // width, cnt % height, b, g, r)
-            #         r, g, b = None, None, None
-            # cnt = cnt + 1
-            
-            # Then scroll image across matrix...
-            image = Image.open("img/muensterhacklogo.png").rotate(90)
-            for n in range(33):  # Start off top-left, move off bottom-right
-                self.matrix.Clear()
-                self.matrix.SetImage(image, n, -1)
+     
+        print("minipicture")
+        # Then scroll image across matrix...
+        image = Image.open("img/muensterhacklogo.png").rotate(90)
+        for n in range(33):  # Start off top-left, move off bottom-right
+            self.matrix.Clear()
+            self.matrix.SetImage(image, n, -1)
+            time.sleep(0.05)
+        for n in range(33):  # Start off top-left, move off bottom-right
+            self.matrix.Clear()
+            self.matrix.SetImage(image, 32 - n, -1)
+            if n < 33:
                 time.sleep(0.05)
-            for n in range(33):  # Start off top-left, move off bottom-right
-                self.matrix.Clear()
-                self.matrix.SetImage(image, 32 - n, -1)
-                time.sleep(0.05)
-
-
-
 
 # Main function
 if __name__ == "__main__":
